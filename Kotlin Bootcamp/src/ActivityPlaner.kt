086051ac1ceb08b741770ec9e188b0e29e55caec
TheHandOfNOD <justme.dude@gmx.net>
2022-04-20 14:31:04 +0200

    changed to inline helper functions and used the double bang operator

--- Kotlin Bootcamp/src/ActivityPlaner.kt
+++ Kotlin Bootcamp/src/ActivityPlaner.kt
@@ -1,12 +1,19 @@
 fun main(args: Array<String>) {
-    println(whatShouldIDoToday("happy"))
-    println(whatShouldIDoToday("happy", "rainy"))
+    println(whatShouldIDoToday(readLine()!!))
+    println(whatShouldIDoToday(readLine()!!, "rainy"))
 }
 
 fun whatShouldIDoToday(mood: String, weather: String = "sunny", temperature: Int = 24): String {
     return when {
-        mood == "happy" && weather == "sunny" -> "go hiking"
-        mood == "happy" && weather == "rainy" -> "workout"
+        isHikingDay(mood, weather) -> "go hiking"
+        isWorkoutDay(mood, weather) -> "workout"
+        isBedDay(mood, weather, temperature) -> "stay in bed"
+        isTimeToSwim(temperature) -> "go swimming"
         else -> "stay home"
     }
 }
+
+fun isHikingDay(mood: String, weather: String) = mood == "happy" && weather == "sunny"
+fun isWorkoutDay(mood: String, weather: String) = mood == "happy" && weather == "rainy"
+fun isBedDay(mood: String, weather: String, temperature: Int) = mood == "sad" && weather == "rainy" && temperature == 10
+fun isTimeToSwim(temperature: Int) = temperature > 35
