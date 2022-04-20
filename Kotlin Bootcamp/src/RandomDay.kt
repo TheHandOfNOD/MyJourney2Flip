96506c05485dda901bf6d4f7829c4fd9a436e2a7
TheHandOfNOD <justme.dude@gmx.net>
2022-04-20 10:48:02 +0200

    First version with when, used as a statement and not as an expression

--- Kotlin Bootcamp/src/RandomDay.kt
+++ Kotlin Bootcamp/src/RandomDay.kt
@@ -8,7 +8,7 @@ fun feedTheFish() {
     val random = Random(1)
     for (i in 0..100) {
         val day = randomDay(random)
-        val food = "pellets"
+        val food = fishFood(day)
         println("Today is $day and the fish eat $food")
     }
 }
@@ -17,3 +17,18 @@ fun randomDay(random: Random): String {
     val week = listOf<String>("Mo", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
     return week[random.nextInt(0,7)]
 }
+
+fun fishFood(day: String): String {
+    var food = "fasting"
+    when (day) {
+        "Mo" -> food = "flakes"
+        "Tue" -> food = "pellets"
+        "Wed" -> food = "redworms"
+        "Thu" -> food = "granules"
+        "Fri" -> food = "mosquitos"
+        "Sat" -> food = "lettuces"
+        "Sun" -> food = "plankton"
+    }
+    return food
+}
+
